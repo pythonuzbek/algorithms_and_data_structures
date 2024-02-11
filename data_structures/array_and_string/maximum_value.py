3ef0476527032c1e126ccb7f8bd0df18afe5dbab
pythonuzbek <nusratullo3304@gmail.com>
2024-02-11 22:46:55 +0500

    add maximum_value

---
+++ data_structures/array_and_string/maximum_value.py
@@ -0,0 +1,11 @@
+def max_integer(array: list):
+    for i in range(0, len(array) - 1):
+        if array[i] > array[i + 1]:
+            temp = array[i]
+            array[i] = array[i + 1]
+            array[i + 1] = temp
+    return array[-1]
+
+
+l = [20, 30, 60, 50, 40]
+print(max_integer(l))
